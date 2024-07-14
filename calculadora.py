e58a92ea753847719ff566d1f6d9ad7a83036fff
Pedro <pedrohf2099@gmail.com>
2024-07-14 18:39:31 -0300

    first

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+a = float(input("Digite o primeiro valor: "))
+
+b = float(input("Digite o segundo valor: "))
+
+soma = a + b
+multiplicação = a*b
+divisão = a/b
+subtração = a-b
+
+print("Resultado da soma: ",soma)
+print("Resultado da multiplicação: ",multiplicação)
+print("Resultado da divisão: " ,divisão)
+print("Resultado da subtração: ",subtração)
